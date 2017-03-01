5db231e504f267827b6ccc673fed5b746b8c9fb1
Vitaly Kuznetsov <vkuznets@redhat.com>
2017-03-01 17:50:48 +0100

    x86/xen: split off smp_pv.c
    Basically, smp.c is renamed to smp_pv.c and some code moved out to common
    smp.c. struct xen_common_irq delcaration ended up in smp.h.
    
    Signed-off-by: Vitaly Kuznetsov <vkuznets@redhat.com>

--- arch/x86/xen/smp.h
+++ arch/x86/xen/smp.h
@@ -17,6 +17,11 @@ extern void xen_smp_intr_free_pv(unsigned int cpu);
 extern void xen_smp_send_reschedule(int cpu);
 extern void xen_smp_send_call_function_ipi(const struct cpumask *mask);
 extern void xen_smp_send_call_function_single_ipi(int cpu);
+
+struct xen_common_irq {
+	int irq;
+	char *name;
+};
 #else /* CONFIG_SMP */
 
 static inline int xen_smp_intr_init(unsigned int cpu)
